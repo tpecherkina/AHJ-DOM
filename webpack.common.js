8b3f77a5c1349f2248f9b32c42083fa5a73f86eb
tpecherkina <tpecherkina18@gmail.com>
2022-05-12 21:27:54 -0400

    corrected

--- webpack.common.js
+++ webpack.common.js
@@ -35,14 +35,7 @@ module.exports = {
         test: /\.svg$/,
         type: 'asset/resource',
       },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader', // 3. Inject styles into DOM
-          'css-loader', // 2. Turns css into commonjs
-          'sass-loader', // 1. Turns sass into css
-        ],
-      },
+   
     ],
 
   },
