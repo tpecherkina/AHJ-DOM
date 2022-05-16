99394db01c41cf5234843f39ef375c30406e32ec
tpecherkina <tpecherkina18@gmail.com>
2022-05-15 20:00:13 -0400

    fixed 3

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,9 @@
-const holes = document.querySelector('.holes');
+const holes = document.querySelector('.holes')
+const hit = document.querySelector('#hit')
+const missed = document.querySelector("#missed")
+let hitTimes = 0
+let missedTimes = 0
+let goblinImgCell 
 
 for (let i = 1; i < 10; i += 1) {
   const hole = document.createElement('div');
@@ -12,16 +17,24 @@ function goblinImg(index, number) {
   if (index !== -1) {
     cells[index].classList.remove('mole');
   }
-  const goblinImgCell = cells[number];
+  goblinImgCell = cells[number];
   goblinImgCell.classList.add('mole');
+
 }
 
-setInterval(() => {
-  const index = cells.findIndex((item) => item.className.includes('mole'));
-  let number = Math.floor(Math.random() * cells.length);
-  if (number === index) {
-    number = Math.floor(Math.random() * cells.length);
-  } else {
-    goblinImg(index, number);
-  }
-}, 1000);
+holes.forEach(hole => {
+  hole.addEventListener('mousedown', () => {
+    if (hole.id == goblinImgCell) {
+      result++
+      hitTimes.textContent = result
+      goblinImgCell = null
+    }
+  })
+    if(hole.id !== goblinImgCell) {
+      result++
+      missedTimes.textContent = result
+      goblinImgCell = null
+    }
+  })
+
+
